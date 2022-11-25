63b965aa7dda219b8c445068639b9332dce4c830
shubham8patni <shubhampatni88@gmail.com>
2022-11-25 14:05:28 +0530

    first commit

---
+++ KNN/KNN.py
@@ -0,0 +1,30 @@
+import numpy as np
+from collections import Counter
+def euclidean_distance(x1, x2):
+    distance = np.sqrt(np.sum((x1-x2)**2))
+    return distance
+
+class KNN:
+    def __init__(self, k=3):
+        self.k = k
+
+    def fit(self, X, y):
+        self.X_train = X
+        self.Y_train = y
+    
+    def predict(self, X):
+        predictions = [self._predict(x) for x in X]
+        return predictions
+
+    def _predict(self, x):
+        # Compute the distances
+        distances = [euclidean_distance(x,x_train) for x_train in self.X_train]
+ 
+        # Get the closest K
+        k_indices = np.argsort(distances)[:self.k]
+        k_nearest_labels = [self.Y_train[i] for i in k_indices]
+
+        # Majority Vote
+        most_common = Counter(k_nearest_labels).most_common()
+        return most_common[0][0]
+
---
+++ KNN/train.py
@@ -0,0 +1,23 @@
+import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from KNN import KNN
+cmap = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+
+iris = datasets.load_iris()
+X,y = iris.data, iris.target
+
+X_train, X_test, Y_train, Y_test = train_test_split(X,y,test_size=0.2, random_state = 1234)
+
+plt.figure()
+plt.scatter(X[:,2], X[:,3], c=y, cmap = cmap, edgecolor='k', s=20)
+plt.show()
+
+# Classifier
+clf = KNN(k=5)
+clf.fit(X_train,Y_train)
+predictions = clf.predict(X_test)
+
+print(predictions)
