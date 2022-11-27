547ac6181a25b6b6361f038eec174867ce88537d
shubham8patni <shubhampatni88@gmail.com>
2022-11-27 20:04:50 +0530

    Linear Regression

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -1,11 +1,12 @@
 import numpy as np
 from collections import Counter
+
 def euclidean_distance(x1, x2):
     distance = np.sqrt(np.sum((x1-x2)**2))
     return distance
 
 class KNN:
-    def __init__(self, k=3):
+    def __init__(self, k):
         self.k = k
 
     def fit(self, X, y):
@@ -19,7 +20,7 @@ class KNN:
     def _predict(self, x):
         # Compute the distances
         distances = [euclidean_distance(x,x_train) for x_train in self.X_train]
- 
+        
         # Get the closest K
         k_indices = np.argsort(distances)[:self.k]
         k_nearest_labels = [self.Y_train[i] for i in k_indices]
---
+++ Linear Regression/linearRegression.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+
+class Linear_regression:
+
+    def __init__(self, lr = 0.001, n_iters = 1000):
+        self.lr = lr
+        self.n_iters = n_iters
+        self.weigths = None
+        self.bias = None
+
+    def fit(self,X,y):
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+
+        for _ in range(n_iters):
+            y_pred = np.dot(X, self.weights) + self.bias
+
+            dw = (1/n_samples) * np.dot(X, (y_pred - y))
+            db = (1/n_samples) * np.sum(y_pred - y)
+
+
+            self.weights = self.weights - self.lr * dw
+            self.bias = self.bias - self.lr * db
+
+    def predict(self, X):
+        y_pred = np.dot(X, self.weights) + self.bias
+        return y_pred
