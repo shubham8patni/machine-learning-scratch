10b56847fcd91b741ca6fcc41862f81a7083a3fa
shubham8patni <shubhampatni88@gmail.com>
2022-11-28 19:44:48 +0530

    initial logistic regression

---
+++ Logistic Regression/LogisticRegression.py
@@ -0,0 +1,27 @@
+import numpy as np 
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+class LogisticRegression:
+    def __init__(self, lr = 0.001, n_iter=1000):
+        self.lr = lr
+        self.n_iter = n_iter
+        self. weights = None
+        self.bias = None
+
+    def fit(self, X, Y):
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(self.features)
+        self.bias = 0
+
+        for _ in range(self.n_iter):
+            linear_pred = np.dot(X, self.weights) + self.bias
+            predictions = sigmoid(linear_pred)
+
+            dw = (1/n_samples) * np.dot(X.T, (predictions - Y))
+            db = (1/n_samples) * np.sum(predictions - Y)
+
+            self.weights = self.weights - self.lr*dw
+            self.bias = self.bias - self.lr*db
+    def predict():
