905a416f26d6e93f232098db82f7dbe0aed61a3e
shubham8patni <shubhampatni88@gmail.com>
2022-11-30 19:16:22 +0530

    updated learning rate

--- Logistic Regression/train.py
+++ Logistic Regression/train.py
@@ -8,7 +8,7 @@ bc = datasets.load_breast_cancer()
 X, y = bc.data, bc.target
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.2, random_state = 1234)
 
-clf = LogisticRegression()
+clf = LogisticRegression(lr=0.01)
 clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test)
 
