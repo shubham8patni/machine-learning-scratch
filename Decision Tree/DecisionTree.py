e1a0ccda79f09061af7dd67c16e8e0d995766a27
shubham8patni <shubhampatni88@gmail.com>
2022-12-02 00:17:04 +0530

    Decision tree initial 2

--- Decision Tree/DecisionTree.py
+++ Decision Tree/DecisionTree.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 
 
 
@@ -15,4 +15,26 @@ class Node:
 
 
 class DecisionTree:
-    def __init__():
+    def __init__(self, min_samples_split = 2, max_depth = 100, n_features = None):
+        self.min_samples_split = min_samples_split
+        self.max_depth = max_depth
+        self.n_features = n_features
+        self.root = None
+
+
+    def fit(self, X, y):
+        self.n_features = X.shape[1] if not self.n_features else min(X.shape[1], self.n_features)
+        self.root = self._grow_tree(X, y)
+
+    def _grow_tree(self, X, y):
+        n_samples, n_feats = X.shape
+        n_labels = np.unique(y)
+
+        # check stopping criteria
+
+        # find best split
+
+        # create child nodes
+
+    
+    def predict():
