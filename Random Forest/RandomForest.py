58a7cf1b01e1d5dfe0913d2b14644a7559cdaf27
shubham8patni <shubhampatni88@gmail.com>
2022-12-09 23:51:33 +0530

    using helper function to get samples in such a way that a value/sample can be repeated

--- Random Forest/RandomForest.py
+++ Random Forest/RandomForest.py
@@ -14,11 +14,14 @@ class RandomForest:
         self.trees = []
         for _ in range(self.n_trees):
             tree = DecisionTree(max_depth=self.max_depth, min_samples_split=self.min_sample_split,n_features=self.n_features)
-            X_sample, y_sample = 
-            tree.fit()
+            X_sample, y_sample = self._bootstrap_samples(X, y)
+            tree.fit(X_sample, y_sample)
+            self.trees.append(tree)
 
 
-    def _bootstrap_samples()
-
+    def _bootstrap_samples(self, X, y):
+        n_samples = X.shape[0]
+        idxs = np.random.choice(n_samples, n_samples, replace = True)
+        return X[idxs], y[idxs]
 
     def predict()
