65c4669b2e807fda751a2a698a58637284e53db2
shubham8patni <shubhampatni88@gmail.com>
2022-12-11 14:58:56 +0530

    completed training 92.1% accuracy

--- Random Forest/train.py
+++ Random Forest/train.py
@@ -4,3 +4,21 @@ from sklearn import datasets
 import matplotlib.pyplot as plt
 from RandomForest import RandomForest
 
+data = datasets.load_breast_cancer()
+X = data.data 
+y = data.target
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.2, random_state = 1234)
+
+
+def accuracy(y_true, y_pred):
+    accuracy = np.sum(y_true == y_pred) / len(y_true)
+    return accuracy
+
+
+clf = RandomForest()
+clf.fit(X_train, y_train)
+predictions = clf.predict(X_test)
+
+acc = accuracy(y_test, predictions)
+print(acc)
